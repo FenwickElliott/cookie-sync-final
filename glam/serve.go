a8f6627873d1c7e96ac1cfef0ec55a96d00110e5
Charles Fenwick Elliott <charles@fenwickelliott.io>
2018-05-14 16:01:33 -0400

    saving assosiations to mongodb

--- glam/serve.go
+++ glam/serve.go
@@ -8,18 +8,36 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
-var port = flag.String("port", "4000", "port to serve on")
+var (
+	port        = flag.String("port", "4000", "port to serve on")
+	mongoServer = flag.String("mongoServer", "127.0.0.1", "mongo server address")
+
+	db  *mgo.Database
+	err error
+)
+
+type association struct {
+	GlamID        string
+	PartnerCookie string
+}
 
 func main() {
+	session, err := mgo.Dial(*mongoServer)
+	check(err)
+	defer session.Close()
+	db = session.DB("partners")
+
 	http.HandleFunc("/in", in)
 	fmt.Println("Serving on port:", *port)
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
 
 func in(w http.ResponseWriter, r *http.Request) {
-
 	r.ParseForm()
 	partnerID := r.FormValue("partner")
 	partnerCookie := r.FormValue("cookieID")
@@ -31,12 +49,21 @@ func in(w http.ResponseWriter, r *http.Request) {
 		check(err)
 	}
 
-	fmt.Println("glamID:", glamCookie.Value)
-	fmt.Println("partnerID:", partnerID)
-	fmt.Println("partnerCookie:", partnerCookie)
+	res := association{}
+	c := db.C(partnerID)
+	err = c.Find(bson.M{"glamid": glamCookie.Value}).One(&res)
+	if err != nil {
+		c.Insert(association{glamCookie.Value, partnerCookie})
+		err = c.Find(bson.M{"glamid": glamCookie.Value}).One(&res)
+	}
+	check(err)
+	if res.PartnerCookie != partnerCookie {
+		panic("partnerCookie doesn't match")
+	}
 }
 
 // Utility functions:
+
 func check(err error) {
 	if err != nil {
 		panic(err)
