ad40df6402f0432d5705ba8d11e54a7234d1161c
Charles Fenwick Elliott <charles@fenwickelliott.io>
2018-05-14 14:01:41 -0400

    cookie.js refactor

--- inception/public/js/cookie.js
+++ inception/public/js/cookie.js
@@ -1,14 +1,29 @@
-let inceptionID = getCookie("inceptionID");
+// Softcode partner specific variables:
+let partnerID = "inception";
+let tracker = "http://localhost:4000/";
 
-if (!inceptionID) {
-    let date = new Date();
-    inceptionID = sha1(window + date);
-    date.setTime(date.getTime()+(365*24*60*60*1000));
-    document.cookie = "inceptionID=" + inceptionID + ";expires="+ date.toUTCString();
+
+
+// Workflow:
+let cookieID = getCookie(partnerID + "ID");
+
+if (!cookieID) {
+    setCookie();
 }
+
+
+
+//Utility functions:
 
 function getCookie(name) {
     let value = "; " + document.cookie;
     let parts = value.split("; " + name + "=");
     if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+function setCookie() {
+    let date = new Date();
+    cookieID = sha1(window + date);
+    date.setTime(date.getTime()+(365*24*60*60*1000));
+    document.cookie = partnerID + "ID=" + cookieID + ";expires="+ date.toUTCString();
 }
